1906df54e98e9b04bfff89af4bc6ca5110c9791f
Alan Shabrandi <alan.shabrandi@gmail.com>
2026-06-26 23:51:19 +0330

    change 1

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,7 @@ app = FastAPI(lifespan=lifespan)
 ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://chatgpt-clone-hazel-nine.vercel.app"
+    "https://ai-assistant-murex-beta.vercel.app"
 ]
 
 FRONTEND_URL = os.getenv("FRONTEND_URL")
